c8e2614a5a915cadae92c1a2b0a1c34aea276d7e
Benjamin Serrano <b.serrano8970@gmail.com>
2021-12-24 12:39:37 -0600

    Creates json file that is within the limits for
    ListenBrainz
    File is then put in a folder and is read.
    Now reads CSV for uploaded music and will update artist name accordingly

--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@ using Newtonsoft.Json;
 using MetaBrainz;
 using MetaBrainz.Common;
 using MetaBrainz.ListenBrainz;
+using Microsoft.VisualBasic.FileIO;
+using System.Text;
 
+string ROOT = "127.0.0.1";
 // string MusicHist = Console.ReadLine();
 string MusicHist = "music-history.json";
 
@@ -16,28 +19,74 @@ string temp = JsonConvert.SerializeObject(History, Formatting.Indented);
 List<JsonLayout> YTHistory = new();
 List<NeededInformation> YTMHistory = new();
 List<NeededInformation> UploadedSongs = new();
+Dictionary<string, string> Artist = new();
+string csvInformation = File.ReadAllText("music-uploads-metadata.csv");
+
+using (TextFieldParser parser = new("music-uploads-metadata.csv"))
+{
+    parser.TextFieldType = FieldType.Delimited;
+    parser.SetDelimiters(",");
+    // Reads first line but promptly ignores it cause it is the header
+    parser.ReadFields();
+    while (!parser.EndOfData)
+    {
+        string[] fields = parser.ReadFields();
+
+        try
+        {
+            string SongTitle = fields[0].Trim();
+            string artist = fields[2].Trim();
+
+            Artist[SongTitle] = artist;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Error");
+        }
+
+    }
+}
 
 File.Create("output.json").Close();
 File.WriteAllText("output.json", temp);
 
-foreach(JsonLayout x in History)
+int i = 0;
+foreach (JsonLayout x in History)
 {
-    if(x.header == "YouTube")
+    if (x.header == "YouTube")
     {
         YTHistory.Add(x);
     }
     else
     {
         NeededInformation ytmInfo = new(x);
-        if(ytmInfo.artist == "Music Library Uploads")
+        if (ytmInfo.artist_name == "Music Library Uploads")
         {
-            UploadedSongs.Add(ytmInfo);
+            if (ytmInfo.track_name != "Ghost" || ytmInfo.track_name != "Prelude")
+            {
+                string artist;
+                Artist.TryGetValue(ytmInfo.track_name, out artist);
+                ytmInfo.artist_name = artist;
+                if(artist == null)
+                {
+                    ytmInfo.artist_name = "";
+                }
+                UploadedSongs.Add(ytmInfo);
+                YTMHistory.Add(ytmInfo);
+            }
         }
         else
         {
             YTMHistory.Add(ytmInfo);
         }
-        
+    }
+    if (Encoding.UTF8.GetByteCount(JsonConvert.SerializeObject(YTMHistory)) > 10200)
+    {
+        temp = JsonConvert.SerializeObject(YTMHistory);
+        File.Create($"JsonOut/{i}.json").Close();
+        File.WriteAllText($"JsonOut/{i}.json", temp);
+        i += 1;
+        YTMHistory.Clear();
     }
 }
 
@@ -83,31 +132,31 @@ public class subtitles
 
 public class NeededInformation
 {
-    public string title { get; set; }
-    public string artist { get; set; }
-    public string time { get; set; }
-
+    public string track_name { get; set; }
+    public string artist_name { get; set; }
+    public long listened_at { get; set; }
     public NeededInformation(JsonLayout temp)
     {
-        char[] charArr = {'-',' ', 'T', 'o', 'p', 'i','c'};
-        title = temp.title;
-        time = temp.time;
-        if(temp.subtitles != null)
+        track_name = temp.title;
+        DateTime _time;
+        DateTime.TryParse(temp.time, out _time);
+        listened_at = ((DateTimeOffset)_time).ToUnixTimeSeconds();
+        if (temp.subtitles != null)
         {
-            artist = temp.subtitles[0].name;
-            if(artist.Contains("- Topic"))
+            artist_name = temp.subtitles[0].name;
+            if (artist_name.Contains("- Topic"))
             {
-                artist = artist.TrimEnd(charArr);
+                int numChar = artist_name.Count();
+                artist_name = artist_name.Remove(numChar - 8, 8);
             }
         }
         else
         {
-            artist = "";
+            artist_name = "";
         }
-        if(title.Contains("Watched "))
+        if (track_name.Contains("Watched "))
         {
-            title = title.Remove(0,8);
+            track_name = track_name.Remove(0, 8);
         }
     }
-
 }
